92e8353e3be3e0d5fd4316e7c02edd1f59d67c3f
Lars_Hansen <leha20@student.aau.dk>
2021-10-30 13:44:19 +0200

    Commented out part of excersisessssees

--- Lecture 1 Classes/Program.cs
+++ Lecture 1 Classes/Program.cs
@@ -7,17 +7,19 @@ namespace Lecture_1_Classes
     {
         static void Main(string[] args)
         {
-            Person mormor = new Person("mormor", "mormor", 70);
-            Person morfar = new Person("morfar", "morfar", 70);
-            Person farfar = new Person("farfar", "farfar", 70);
-            Person farmor = new Person("farmor", "farmor", 70);
-            Person poul = new Person("poul", "hansen", 55, farmor, farfar);
-            Person pauline = new Person("pauline", "hansen", 42, mormor, morfar);
-            Person lars = new Person("lars", "hansen", 22, pauline, poul);
-
+            //Person mormor = new Person("mormor", "mormor", 70);
+            //Person morfar = new Person("morfar", "morfar", 70);
+            //Person farfar = new Person("farfar", "farfar", 70);
+            //Person farmor = new Person("farmor", "farmor", 70);
+            //Person poul = new Person("poul", "hansen", 55, farmor, farfar);
+            //Person pauline = new Person("pauline", "hansen", 42, mormor, morfar);
+            //Person lars = new Person("lars", "hansen", 22, pauline, poul);
+            //
             //PersonPrinter.PrintPerson(lars);
             //PersonPrinter.PrintAncestry(lars);
-
+            //
+            //---------------------------------------------------------
+            //
             DirectoryInfo di = new DirectoryInfo(@"D:\DirectoryTest");
             DirectoryInfo[] dirs = di.GetDirectories();
 
