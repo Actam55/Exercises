3f10171769b4bf9b3cce5456bd3c839760812f36
Lars_Hansen <leha20@student.aau.dk>
2021-11-08 18:13:32 +0100

    Started work on Lecture 8

---
+++ Lecture 8 Collections/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lecture_8_Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sequence list = new Sequence(1, 2, 4);
+            Sequence.ShowSequence(list);
+        }
+    }
+}
---
+++ Lecture 8 Collections/Sequence.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_8_Collections
+{
+    public class Sequence : IEnumerable<int>
+    {
+        public Sequence(int start, int skip, int count)
+        {
+            Start = start;
+            Skip = skip;
+            Count = count;
+        }
+
+        public List<int> data { get; set;}
+        public int Start { get; set; }
+        public int Skip { get; set; }
+        public int Count { get; set; }
+
+        public static void ShowSequence(IEnumerable<int> e)
+        {
+            IEnumerator<int> i = e.GetEnumerator();
+            
+
+
+
+            //int n = 0;
+            //List<int> list = new List<int>();
+            //for (int i = 0; i < Count; i++)
+            //{
+            //    if (i == 0)
+            //    {
+            //        list.Add(Start);
+            //        i = Start;
+            //    }
+            //    else if (i % Skip == 0)
+            //    {
+            //
+            //    }
+            //    else
+            //    {
+            //        list.Add(i);
+            //    }
+            //
+            //    //Console.WriteLine(i);
+            //}
+            //return list;
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            return data.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        //public static void Start(IEnumerable<int> i)
+        //{
+        //    IEnumerator<int> p = i.GetEnumerator();
+        //    p.MoveNext();
+        //    Console.WriteLine(p.Current);
+        //}
+        //public static void Count(IEnumerable<int> i)
+        //{
+        //    int n = 0;
+        //    IEnumerator<int> p = i.GetEnumerator();
+        //    while (p.MoveNext())
+        //    {
+        //        n++;
+        //    }
+        //    Console.WriteLine(n);
+        //}
+    }
+}
